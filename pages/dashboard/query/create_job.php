4f26c14c6db8372d4d76d451cdccf7b1fa7a0d31
JanzenDC <janzendelacruz28@gmail.com>
2024-11-14 13:01:31 +0800

    "Updated create_job.php to allow user_type '3' in addition to '2' for admin access."

--- pages/dashboard/query/create_job.php
+++ pages/dashboard/query/create_job.php
@@ -3,10 +3,11 @@ session_start();
 require '../../../backend/db_connect.php';
 
 // Check if the user is logged in and is an admin
-if (!isset($_SESSION['user']) || $_SESSION['user']['user_type'] !== '2') {
+if (!isset($_SESSION['user']) || ($_SESSION['user']['user_type'] !== '2' && $_SESSION['user']['user_type'] !== '3')) {
     echo json_encode(['success' => false, 'message' => 'Unauthorized.']);
     exit;
 }
+
 
 // Get data from POST request
 $title = $_POST['job_title'];
